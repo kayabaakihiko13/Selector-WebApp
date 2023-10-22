3a5b1be552f0dba9ce39d766abc0b5b860ff808d
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-10-22 23:07:15 +0700

    update: update ui dan functional

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
-import os
+from fastapi.staticfiles import StaticFiles
+from image_classification import image_to_array, result_waifu_generate
 
 app = FastAPI()
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 templates = Jinja2Templates(directory="templates")
 
 class ImageUploadService:
@@ -12,9 +15,10 @@ class ImageUploadService:
         self.upload_directory = upload_directory
 
     def save_image(self, file: UploadFile):
-        with open(os.path.join(self.upload_directory, file.filename), "wb") as image_file:
+        image_path = os.path.join(self.upload_directory, file.filename)
+        with open(image_path, "wb") as image_file:
             image_file.write(file.file.read())
-        return file.filename
+        return image_path
 
 image_upload_service = ImageUploadService("uploads/")
 
@@ -23,9 +27,11 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/upload/")
-async def upload_image(request: Request, image: UploadFile = File(...)):
+async def upload_images(request: Request, image: UploadFile = File(...)):
     image_filename = image_upload_service.save_image(image)
-    return {"message": f"Gambar {image_filename} berhasil diunggah"}
+    image_arr = image_to_array(image_filename)
+    prediction = result_waifu_generate(image_arr)
+    return JSONResponse(content={"prediction": prediction, "image_filename": f"/uploads/{os.path.basename(image_filename)}"})
 
 if __name__ == "__main__":
     import uvicorn
